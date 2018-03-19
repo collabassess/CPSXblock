2b17054818fced9daa73282d478f71b634ce5f63
Ajay Thorve <ajaythorve1994@gmail.com>
2018-03-19 02:37:06 -0400

    Minor update 2

--- cpsxblock/cpsxblock.py
+++ cpsxblock/cpsxblock.py
@@ -123,9 +123,9 @@ class CPSXBlock(StudioEditableXBlockMixin,XBlock):
         user1 = data['partner']
         user2 = self.get_userid()
         course = self.returnCourseId()
-        data2 = {'user1':user1, 'user2':user2, 'course_id':course}
+        content = {'user1':user1, 'user2':user2, 'course_id':course}
         response = requests.post("http://ec2-54-156-197-224.compute-1.amazonaws.com:3000/onlinePool/pairUsers",
-                                 json=data2)
+                                 json=content)
         room = str(response.text)
         return {"room": room, "size": self.Group_Size, "s_id": self.get_userid(), "s_session": room}
 
