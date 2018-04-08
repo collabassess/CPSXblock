e32062a27d9043830ba4b8a73f3de88dea868c7d
Ajay Thorve <ajaythorve1994@gmail.com>
2018-04-08 12:35:43 -0400

    Updated sql dump file

--- Database file/collab_assess.sql
+++ Database file/collab_assess.sql
@@ -24,6 +24,19 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `collab_assess` /*!40100 DEFAULT CHARAC
 USE `collab_assess`;
 
 --
+-- Temporary view structure for view `get_available_partners`
+--
+
+DROP TABLE IF EXISTS `get_available_partners`;
+/*!50001 DROP VIEW IF EXISTS `get_available_partners`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `get_available_partners` AS SELECT 
+ 1 AS `user_id`,
+ 1 AS `gender`*/;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Table structure for table `user_groups`
 --
 
@@ -31,22 +44,16 @@ DROP TABLE IF EXISTS `user_groups`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_groups` (
-  `group_id` int(11) NOT NULL AUTO_INCREMENT,
-  `course_id` varchar(30) NOT NULL DEFAULT '',
-  `user1` varchar(30) DEFAULT NULL,
-  `user2` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`group_id`,`course_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `session_id` int(11) NOT NULL AUTO_INCREMENT,
+  `course_id` varchar(60) NOT NULL DEFAULT '',
+  `user1` varchar(60) DEFAULT NULL,
+  `user2` varchar(60) DEFAULT NULL,
+  `status` varchar(60) NOT NULL DEFAULT 'valid',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`session_id`,`course_id`),
+  UNIQUE KEY `session_id` (`session_id`,`course_id`,`user1`,`user2`,`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_groups`
---
-
-LOCK TABLES `user_groups` WRITE;
-/*!40000 ALTER TABLE `user_groups` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_groups` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user_hint_solutions`
@@ -64,13 +71,126 @@ CREATE TABLE `user_hint_solutions` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user_hint_solutions`
+-- Table structure for table `user_info`
 --
 
-LOCK TABLES `user_hint_solutions` WRITE;
-/*!40000 ALTER TABLE `user_hint_solutions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_hint_solutions` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `user_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_info` (
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `gender` varchar(30) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_status`
+--
+
+DROP TABLE IF EXISTS `user_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_status` (
+  `user_id` int(11) NOT NULL,
+  `status` varchar(30) DEFAULT 'online',
+  `last_online` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `grouped` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `after_user_status_update` AFTER UPDATE
+    ON `user_status`
+    FOR EACH ROW BEGIN
+        IF NEW.status="offline"
+            THEN UPDATE user_groups SET status='invalid' WHERE (user1=New.user_id or user2=New.user_id) AND status="valid";
+        END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Dumping events for database 'collab_assess'
+--
+/*!50106 SET @save_time_zone= @@TIME_ZONE */ ;
+/*!50106 DROP EVENT IF EXISTS `user_session_invalid` */;
+DELIMITER ;;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;;
+/*!50003 SET character_set_client  = utf8 */ ;;
+/*!50003 SET character_set_results = utf8 */ ;;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;;
+/*!50003 SET @saved_time_zone      = @@time_zone */ ;;
+/*!50003 SET time_zone             = 'SYSTEM' */ ;;
+/*!50106 CREATE*/ /*!50117 DEFINER=`root`@`localhost`*/ /*!50106 EVENT `user_session_invalid` ON SCHEDULE EVERY 1 HOUR STARTS '2018-03-19 05:11:56' ON COMPLETION NOT PRESERVE ENABLE COMMENT 'clear session of paired users who have spent more than 3 hours' DO UPDATE user_groups SET status="invalid" WHERE status='valid' AND created_at <= Now() - INTERVAL 180 Minute */ ;;
+/*!50003 SET time_zone             = @saved_time_zone */ ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;;
+/*!50003 SET character_set_results = @saved_cs_results */ ;;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;;
+/*!50106 DROP EVENT IF EXISTS `user_status_offline` */;;
+DELIMITER ;;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;;
+/*!50003 SET character_set_client  = utf8 */ ;;
+/*!50003 SET character_set_results = utf8 */ ;;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;;
+/*!50003 SET @saved_time_zone      = @@time_zone */ ;;
+/*!50003 SET time_zone             = 'SYSTEM' */ ;;
+/*!50106 CREATE*/ /*!50117 DEFINER=`root`@`localhost`*/ /*!50106 EVENT `user_status_offline` ON SCHEDULE EVERY 1 MINUTE STARTS '2018-03-15 02:53:31' ON COMPLETION NOT PRESERVE ENABLE COMMENT 'clear online pool of users who have' DO UPDATE user_status SET status='offline' WHERE status='online' AND last_online <= Now() - INTERVAL 5 Minute */ ;;
+/*!50003 SET time_zone             = @saved_time_zone */ ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;;
+/*!50003 SET character_set_results = @saved_cs_results */ ;;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;;
+DELIMITER ;
+/*!50106 SET TIME_ZONE= @save_time_zone */ ;
+
+--
+-- Dumping routines for database 'collab_assess'
+--
+
+--
+-- Current Database: `collab_assess`
+--
+
+USE `collab_assess`;
+
+--
+-- Final view structure for view `get_available_partners`
+--
+
+/*!50001 DROP VIEW IF EXISTS `get_available_partners`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `get_available_partners` AS select `s`.`user_id` AS `user_id`,`i`.`gender` AS `gender` from (`user_status` `s` join `user_info` `i`) where ((`s`.`user_id` = `i`.`user_id`) and (`s`.`status` = 'online') and (`s`.`grouped` = 0)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -81,4 +201,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-21  1:19:50
+-- Dump completed on 2018-04-08 16:29:36
