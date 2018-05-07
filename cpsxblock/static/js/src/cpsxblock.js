34d6d19d1f067cba71041585e4b19b0edd2af209
Ajay Thorve <ajaythorve1994@gmail.com>
2018-05-07 19:17:33 -0400

    udpate

--- cpsxblock/static/js/src/cpsxblock.js
+++ cpsxblock/static/js/src/cpsxblock.js
@@ -348,7 +348,7 @@ function CPSXBlock(runtime, element,data) {
     }
     $(function ($) {
             //getAvailableUsers();
-            selector = $("#problem_d0413bf128374e90889b1a151aeec014 .problem");
+            selector = $("#problem_d0413bf128374e90889b1a151aeec014 .problem div .wrapper-problem-response #inputtype_d0413bf128374e90889b1a151aeec014_2_1");
             value = selector.text();
 
             selector.bind('DOMNodeInserted',sendAnswers);
